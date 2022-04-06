04b2b73f2f1090b5809af7a4733c9fdf696faf28
desmond <desmond@desmonds-MacBook-Pro.local>
2022-04-06 12:49:06 +0300

    task compleated

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
-function scuberGreetingForFeet(){
+
+    function scuberGreetingForFeet(freeSample) {
+      // Write your code here!
+
+      if (freeSample <= 400){
+              return "This one is on me!";
+      }
+      else if (freeSample > 400 && freeSample<2000){
+        return 'That will be twenty bucks.'
+      }
+      else if (freeSample > 2000 && freeSample < 2500){
+          return "I will gladly take your thirty bucks.";
+      }
+      else if(freeSample > 2500){
+          return "No can do.";
+      }
+  
+  }
+  
+function ternaryCheckCity(city) {
   // Write your code here!
+
+  if (city === "NYC") {
+      return "Ok, sounds good.";
+  }
+  else{
+      return "No go.";
+  }
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(generousTip){
   // Write your code here!
+  switch(generousTip){
+    case 'generous':
+      return 'Thank you so much.';
+      case 'not as generous':
+        return 'Thank you.';
+        default: 
+        return 'Bye.';
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
